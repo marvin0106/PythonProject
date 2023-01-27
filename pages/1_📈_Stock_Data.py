333a6191e53f30e2eac33e66bd6cac6bdd3b2140
marvin0106 <123030031+marvin0106@users.noreply.github.com>
2023-01-27 19:18:00 +0100

    Add files via upload

---
+++ pages/1_📈_Stock_Data.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  1 22:41:42 2023
+
+@author: marvinsilvafortes
+"""
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+
+
+# Get stock data from yfinance
+def get_stock_data(ticker, period):
+  stock_data = yf.Ticker(ticker).history(period=period)
+  return stock_data
+
+# Create a main function
+def main():
+  # Create a page called "Favourite Stocks"
+  st.sidebar.title("Stock Picker")
+  st.sidebar.text("Enter a ticker symbol to select a stock:")
+  ticker_symbol = st.sidebar.text_input("Ticker symbol", value = "AAPL")
+  st.sidebar.text("Select a period for the stock data:")
+  period_options = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
+
+  period = st.sidebar.selectbox("Period", period_options, index=5)
+
+
+  # Display stock chart and data
+  if ticker_symbol:
+    st.title(str.upper(ticker_symbol) + " - Stock Data")
+    stock_data = get_stock_data(ticker_symbol, period)
+    if "Date" in stock_data.columns:
+      stock_data.reset_index(inplace=True)    
+      stock_data.set_index("Date", inplace=True, date_format="YYYY-MM-DD")
+    st.line_chart(stock_data["Close"])
+    stock_data_table = stock_data[["Close"]]
+    stock_data_table.rename(columns={"Close": "Closing Price"}, inplace=True)
+    #st.markdown(yf.Ticker(ticker_symbol).info["52WeekChange"])
+    
+    with st.expander("Open to see Stock Data on Closing Price per Date "): 
+        st.dataframe(stock_data_table)
+        # Create an Excel file with the data displayed in the dataframe
+        if st.button("Create Excel file"):
+          file_name = f"{ticker_symbol}_{period}.xlsx"
+          stock_data_table.to_excel(file_name)
+          st.success(f"Excel file '{file_name}' created successfully.")
+          
+          
+    # ======== MAJOR STAKEHOLDERS; EVTL COLUMN ANPASSEN; ICH BEKOMMS NICHT HIN
+    stakeholders = pd.DataFrame()
+    stakeholders = pd.DataFrame(stakeholders, columns = ['Stakeholder', 'Data'])
+    stakeholders = yf.Ticker(ticker_symbol).major_holders
+    #stakeholders_df = pd.DataFrame(columns = ['Stakeholder', 'Data'])
+    st.dataframe(stakeholders)
+    
+    
+    # ======== NEWS TICKER; WOLLTE LINKS CLICKABLE MACHEN, Geht nicht als Table
+    st.header(f"Recent Headlines associated with {ticker_symbol.upper()}")
+    News = yf.Ticker(ticker_symbol).news
+    news_df = pd.DataFrame.from_records(News)    
+    news_df = news_df[['title', 'publisher', 'link',]]
+    news_df = news_df.rename(columns={'title': 'Title', 'publisher': 'Publisher', 'link': 'Link'})
+    # def make_clickable(link):
+    #     # target _blank to open new window
+    #     # extract clickable text to display for your link
+    #     text = link.split('=')[1]
+    #     return f'<a target="_blank" href="{link}">{text}</a>'
+    
+    # news_df.iloc[:,2] = news_df.iloc[:,2].apply(make_clickable)
+    # news_df = news_df.to_html(escape=False)
+    # st.write(news_df, unsafe_allow_html=True)
+    
+    #st.table(news_df.to_html(index=False))    
+    
+    st.table(news_df)
+    
+    
+    # Create an Excel file with the data displayed in the dataframe
+      
+    selected_stock = str.upper(ticker_symbol)
+    stock_data = yf.Ticker(selected_stock)
+    
+# =============================================================================
+#     #additional information feature in sidebar
+#     
+# =============================================================================
+
+    st.sidebar.subheader("""Display Additional Information""")
+    #checkbox to display stock actions for the searched ticker
+    actions = st.sidebar.checkbox("Stock Actions")
+    if actions:
+        st.subheader("""Stock **actions** for """ + selected_stock)
+        display_action = (stock_data.actions)
+        if display_action.empty == True:
+            st.write("No data available at the moment")
+        else:
+            st.write(display_action)
+    
+    #checkbox to display quarterly financials for the searched ticker
+    financials = st.sidebar.checkbox("Quarterly Financials")
+    if financials:
+        st.subheader("""**Quarterly financials** for """ + selected_stock)
+        display_financials = (stock_data.quarterly_financials)
+        if display_financials.empty == True:
+            st.write("No data available at the moment")
+        else:
+            st.write(display_financials)
+    
+    #checkbox to display list of institutional shareholders for searched ticker
+    major_shareholders = st.sidebar.checkbox("Institutional Shareholders")
+    if major_shareholders:
+        st.subheader("""**Institutional investors** for """ + selected_stock)
+        display_shareholders = (stock_data.institutional_holders)
+        if display_shareholders.empty == True:
+            st.write("No data available at the moment")
+        else:
+            st.write(display_shareholders)
+    
+    #checkbox to display quarterly balance sheet for searched ticker
+    balance_sheet = st.sidebar.checkbox("Quarterly Balance Sheet")
+    if balance_sheet:
+        st.subheader("""**Quarterly balance sheet** for """ + selected_stock)
+        display_balancesheet = (stock_data.quarterly_balance_sheet)
+        if display_balancesheet.empty == True:
+            st.write("No data available at the moment")
+        else:
+            st.write(display_balancesheet)
+    
+    #checkbox to display quarterly cashflow for searched ticker
+    cashflow = st.sidebar.checkbox("Quarterly Cashflow")
+    if cashflow:
+        st.subheader("""**Quarterly cashflow** for """ + selected_stock)
+        display_cashflow = (stock_data.quarterly_cashflow)
+        if display_cashflow.empty == True:
+            st.write("No data available at the moment")
+        else:
+            st.write(display_cashflow)
+    
+    #checkbox to display quarterly earnings for searched ticker
+    earnings = st.sidebar.checkbox("Quarterly Earnings")
+    if earnings:
+        st.subheader("""**Quarterly earnings** for """ + selected_stock)
+        display_earnings = (stock_data.quarterly_earnings)
+        if display_earnings.empty == True:
+            st.write("No data available at the moment")
+        else:
+            st.write(display_earnings)
+    
+    #checkbox to display list of analysts recommendation for searched ticker
+    analyst_recommendation = st.sidebar.checkbox("Analysts Recommendation")
+    if analyst_recommendation:
+        st.subheader("""**Analysts recommendation** for """ + selected_stock)
+        display_analyst_rec = (stock_data.recommendations)
+        st.write(display_analyst_rec)
+
+    sustainability = st.sidebar.checkbox("Sustainability")
+    if sustainability:
+        st.subheader("""**Data on Sustainabilility** for """ + selected_stock)
+        display_sustainability = stock_data.sustainability
+        st.write(display_sustainability)
+
+# Run the main function
+if __name__ == "__main__":
+  main()
---
+++ pages/3_⏳_Most Recent.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 21 16:13:51 2023
+
+@author: marvinsilvafortes
+"""
+
+import pandas as pd
+import streamlit as st
+import yfinance as yf
+import matplotlib.pyplot as plt
+import streamlit.components.v1 as components
+import mpld3
+import locale
+import datetime
+
+
+plt.style.use("seaborn-darkgrid")
+print(plt.style.available)
+
+st.sidebar.title("Stock Filter")
+companies_input = st.sidebar.text_input("Enter a comma-separated list of company tickers:", "")
+companies_input = companies_input.replace(" ", "")
+companies = companies_input.split(",")
+
+# Function for translating tickers into names: 
+
+#def get_company_name(ticker):
+#    company_info = yf.Ticker(ticker).info
+#    name = company_info["longName"]
+#    return name
+
+#print(yf.Ticker("msft").info["longName"])
+#print(get_company_name("msft"))
+
+# =============================================================================
+# Text Section
+# =============================================================================
+
+st.title("1000 Most Recent Insider Trades")
+st.markdown("This Table contains the most recent SEC filings for insider Trading Buys & Sells")
+st.markdown("Option exercises are not considered")
+
+
+
+with st.sidebar: 
+    st.sidebar.title("Name Search")
+    Stocklist = pd.read_excel("Stocklist.xlsx")
+    StocksnTickers = Stocklist.iloc[1:, [0,1]]
+    StocksnTickers.columns = ['Ticker', 'Company Name']
+    
+    search_term = st.sidebar.text_input("Ticker or Company Name Search")
+    
+    filtered_df = StocksnTickers[(StocksnTickers['Company Name'].str.contains(search_term, case=False, na=False)) | (StocksnTickers['Ticker'].str.contains(search_term, case=False, na=False))]
+    st.dataframe(filtered_df)
+
+# =============================================================================
+
+# Filter Box: 
+if companies_input:
+    st.success("Stock Filter for: " + companies_input.upper())
+#    st.markdown(get_company_name(companies_input))
+else:
+    st.warning("Enter a comma-separated list of company tickers to filter")
+    
+tab1, tab2, tab3 = st.tabs(["Table", "Chart", "Performance"])
+
+# Scraping & Implementation:
+insider = pd.DataFrame()
+
+for company in companies: 
+
+    url = f'http://openinsider.com/screener?s={company}&o=&pl=&ph=&ll=&lh=&fd=1461&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=1000&page=1'
+
+    insider1 = pd.read_html(url)
+    try:
+        insider1 = insider1[-3]
+        insider = pd.concat([insider, insider1])
+        insider = insider.drop(columns=["X", "1d", "1w", "1m", "6m"])
+        
+        insider = insider.reset_index(drop=True)
+    
+    
+        with tab1:
+            st.table(insider)
+        with tab2: 
+            st.markdown("Stock Chart from filtered Stock:")  
+            try:
+                def get_stock_data(ticker, period):
+                    stock_data = yf.Ticker(ticker).history(period=period)
+                    return stock_data
+            except NameError:
+                st.warning("Stock filter not applied, therefore no chart can be displayed here.")
+                    
+    # Stock Chart
+  
+            ticker_symbol = company
+            
+            # Get the selected interval
+            interval = 4
+            
+            # Get the current date
+            now = pd.to_datetime('now')
+            
+            # Calculate the start date of the interval
+            four_months_ago = now - pd.DateOffset(months=interval)
+
+            insider.iloc[:, 1] = pd.to_datetime(insider.iloc[:, 1], format="%Y-%m-%d")
+            # Filter the insider dataframe to only include rows where the date is within the selected interval
+            filtered_insider = insider[(insider.iloc[:,1] >= four_months_ago) & (insider.iloc[:,1] <= now)]
+
+        
+            # Use the filtered dataframe to define the x_values
+            x_values = filtered_insider.iloc[:,1]
+            
+            
+            periodx = f"{interval}y"
+
+            if ticker_symbol:
+              st.header(str.upper(ticker_symbol) + " - Stock Data - 4y")
+              stock_data = get_stock_data(ticker_symbol, periodx)
+              if "Date" in stock_data.columns:
+                stock_data.reset_index(inplace=True)    
+                stock_data.set_index("Date", inplace=True, date_format="YYYY-MM-DD")
+            try: 
+                st.line_chart(stock_data['Close'])
+            except NameError:
+                st.warning("Stock filter not applied, therefore no chart can be displayed here.")
+            
+            
+            
+            insider.iloc[:,1] = pd.to_datetime(insider.iloc[:,1])
+            purchase_df = insider[insider.iloc[:,5].str.contains("Purchase")]
+            purchase_df.to_csv("chosen_purchases.csv")
+            sale_df = insider[insider.iloc[:,5].str.contains("Sale")]
+            
+            #now = pd.to_datetime("now")
+            #four_months_ago = now - pd.DateOffset(days=start_date)
+            
+            #purchase_df = purchase_df[(purchase_df.iloc[:,1] > four_months_ago) & (purchase_df.iloc[:,1] <= now)]
+            
+            
+            
+            print(purchase_df)
+            st.header("Table with Insider-Purchases")
+            st.table(purchase_df)
+            
+            print(sale_df)
+            st.header("Table with Insider-Sales:")
+            st.table(sale_df)
+            
+            insider.iloc[:,1] = pd.to_datetime(insider.iloc[:,1])
+            
+            # Plot the "Close" price over time
+            fig = plt.figure(figsize = (9,5), dpi=200)
+            try:
+                plt.plot(stock_data["Close"], color="k", linewidth = "1")
+            except NameError: 
+                st.warning("Stock filter not applied, therefore no chart can be displayed here.")
+            plt.xlabel("Date")
+            plt.ylabel("Price in $")
+            plt.title(f"{ticker_symbol} ".upper()+ "Price", fontsize=16)
+            plt.xticks(rotation=40, fontsize = 12, alpha=0.7)
+            
+            # Beauty Program for Plots: 
+            plt.gca().spines["top"].set_alpha(0.0)    
+            plt.gca().spines["bottom"].set_alpha(0.3)
+            plt.gca().spines["right"].set_alpha(0.0)    
+            plt.gca().spines["left"].set_alpha(0.3)   
+            
+            fig_html = mpld3.fig_to_html(fig)
+            # For added Interactivity remove "#"
+            #components.html(fig_html, height = 600)
+            
+
+            for index, row in purchase_df.iterrows():
+                date = pd.to_datetime(row[1])
+                plt.axvline(x=date, color="b", linestyle="dotted", linewidth=".8")
+            
+            for index, row in sale_df.iterrows():
+                date = pd.to_datetime(row[1])
+                plt.axvline(x=date, color="r", linestyle="dotted", linewidth=".8")
+            
+            st.pyplot(fig)
+            plt.show()
+            
+        with tab3:
+            purchase_df = purchase_df.drop(columns=["Filing\xa0Date", "Owned", "ΔOwn"])
+            purchase_df["Trade\xa0Date"] = purchase_df["Trade\xa0Date"].dt.date
+            purchase_df.rename(columns={'Trade\xa0Date': 'Date'}, inplace=True)
+
+            #st.table(purchase_df)
+            
+            #New separate DF to save Data on tickers through yfinance API
+            new_df = pd.DataFrame(columns=['Date', 'Ticker', 'Closing Price'])
+            
+            locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' )
+            
+            for index, row in purchase_df.iterrows():
+                ticker = row['Ticker']
+                date = row['Date']
+                old_price = row["Price"]
+                date = date.strftime("%Y-%m-%d")
+                
+                # Download data
+                stock_data = yf.download(ticker, start="2010-01-01", end=date, interval = "1d",threads=False)
+                stock_data = stock_data.tail(1)
+                stock_price = round(stock_data.loc[:, "Adj Close"].values[0], 2)
+                purchase_df.loc[index, "Price"] = "{:.2f}".format(stock_price)
+
+                
+                
+                # Get today's date - Automatically takes yesterdays closing 
+                today = datetime.date.today()
+                
+                # Download data
+                stock_data = yf.download(ticker, start="2010-01-01", end=today, interval = "1d")
+                stock_data = stock_data.tail(1)
+
+                stock_data.reset_index(inplace=True)    
+                closing_price=stock_data["Adj Close"]
+                closing_price = stock_data.iat[0,2]
+                closing_price = locale.currency( closing_price, grouping = True )
+                
+                
+                new_df = new_df.append({'Date': date, 'Ticker': ticker, 'Closing Price': closing_price}, ignore_index=True)
+            
+            
+            purchase_df["Ticker"] = purchase_df["Ticker"].astype(str)
+            purchase_df["Date"] = pd.to_datetime(purchase_df["Date"], format='%Y-%m-%d')
+            new_df["Ticker"] = new_df["Ticker"].astype(str)
+            new_df["Date"] = pd.to_datetime(new_df["Date"], format='%Y-%m-%d')
+            
+            # Cleaned up Dates to look better in Dataframe: 
+            purchase_df["Date"] = purchase_df["Date"].dt.strftime('%Y-%m-%d')
+            new_df["Date"] = new_df["Date"].dt.strftime('%Y-%m-%d')
+            
+            # merge the two dataframes using outer join
+            merged_df = pd.merge(purchase_df, new_df, on=["Ticker", "Date"], how='outer')
+            
+            
+            merged_df.columns = merged_df.columns.str.replace(' ', '_')
+            
+            
+            merged_df['Price'] = pd.to_numeric(merged_df['Price'].str.replace('$',''))
+            merged_df['Closing_Price'] = pd.to_numeric(merged_df['Closing_Price'].str.replace('$',''))
+            
+            merged_df['Percent_Change'] = (merged_df['Closing_Price'] - merged_df['Price'])/merged_df['Price']*100
+
+            # Section where we introduce alpha: ===============================
+            sp500 = pd.read_csv("/Users/marvinsilvafortes/Desktop/7. Semester/Econ mit Python x/Project/pages/sp500_index.csv")
+           
+            df_split = sp500['Date;S&P500'].str.split(";", expand=True)
+            df_split.columns = ['Date', 'S&P500']
+            sp500 = pd.concat([sp500, df_split], axis=1)
+            sp500.drop(columns=["Date;S&P500"], inplace=True)
+            sp500['Date'] = pd.to_datetime(sp500['Date'], format='%d.%m.%Y').dt.strftime('%Y-%m-%d')
+            
+            # convert 'Date' column in sp500 to datetime format
+            sp500['Date'] = pd.to_datetime(sp500['Date'])
+            sp500['Date'] = pd.to_datetime(sp500['Date'], format='%Y-%m-%d')
+            
+            
+            # change format of 'Date' column in merged_df to match the format in sp500
+            merged_df['Date'] = pd.to_datetime(merged_df['Date'])
+            #merged_df['Date'] = merged_df['Date'].dt.strftime('%Y-%m-%d')
+            merged_df['Date'] = pd.to_datetime(merged_df['Date'], format='%Y-%m-%d')
+
+            merged_df = pd.merge(merged_df, sp500, on='Date')
+
+            # ==End SNP500 value formatting and integrating
+            merged_df['S&P500'] = pd.to_numeric(merged_df['S&P500'], errors='coerce')
+
+            # Get the S&P500 value for the newest available date in the sp500 dataframe
+            latest_sp500_value = sp500.loc[sp500['Date'] == sp500['Date'].max(), 'S&P500'].values[0]
+            latest_sp500_value = pd.to_numeric(latest_sp500_value, errors='coerce')
+
+            
+            # Create a new column in the merged_df dataframe that calculates the percentage change
+            merged_df['S&P500_PCT_Change'] = ( latest_sp500_value - merged_df['S&P500']) / merged_df['S&P500'] * 100
+            merged_df["Alpha"] = merged_df["Percent_Change"] - merged_df["S&P500_PCT_Change"]
+ 
+            
+
+            print(merged_df)
+            
+            # END OF ALPHA ====================================================
+
+            # Transform back before putting into Table:             
+            merged_df['Price'] = merged_df['Price'].astype(str)
+            merged_df['Closing_Price'] = merged_df['Closing_Price'].astype(str)
+            merged_df['Price'] = "$"+merged_df['Price']
+            merged_df['Closing_Price'] = "$"+merged_df['Closing_Price']
+            merged_df = merged_df.rename(columns={'Closing_Price': 'Closing_Price_Today'})
+
+            merged_df['Percent_Change'] = merged_df['Percent_Change'].round(2)
+            merged_df['Percent_Change'] = merged_df['Percent_Change'].apply('{:.2f}%'.format)
+            
+
+            
+            # Take or Drop SnP500 at Trade Date 
+            #merged_df = merged_df.drop("S&P500", axis=1)
+            merged_df['S&P500'] = merged_df['S&P500'].round(2)
+            
+            merged_df['S&P500_PCT_Change'] = merged_df['S&P500_PCT_Change'].round(2)
+            merged_df['S&P500_PCT_Change'] = merged_df['S&P500_PCT_Change'].apply('{:.2f}%'.format)
+            
+            merged_df['Alpha'] = merged_df['Alpha'].round(2)
+            merged_df['Alpha'] = merged_df['Alpha'].apply('{:.2f}%'.format)
+            merged_df.columns = merged_df.columns.str.replace('_', ' ')
+            
+            st.header("Purchase Performance per Trade")
+            st.table(merged_df)
+                   
+            
+            
+    except KeyError:
+        st.warning("Entered Ticker Filter <" + company.upper() +"> is Invalid or does not exist")
+
+insider.to_csv("chosen_companies.csv")
+
